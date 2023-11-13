ca6d50e8374e0ffd488504d33148c4ec0a271d15
Daniel Riyanto <danielriyanto190700@gmail.com>
2023-11-13 13:54:31 +0700

    feat(FSB3-12): Create CRUD Restful API of Inventory Management
    - Create `database/db.json`
      Contain the data
    - Create index.js
      Contain the routes and controllers
    - Create ReadMe.md
      Contain documentation about the APIs

---
+++ ReadMe.md
@@ -0,0 +1,248 @@
+# Basic Express
+
+Basic express.js project with basic routes:
+* Express
+* Joi
+* Fs
+
+---
+
+## How to Run
+```
+npm start
+```
+
+## URL
+
+_Server_
+```
+http://localhost:3000
+```
+---
+
+
+## Global Response
+
+_Response (500 - Internal Server Error)_
+```
+{
+  "message": "Internal Server Error"
+}
+```
+---
+
+
+## RESTful endpoints
+
+### GET /products
+
+> Get all products
+
+_Request Header_
+```
+not needed
+```
+
+_Request Body_
+```
+not needed
+```
+
+_Response (200)_
+```
+{
+  "data": [
+    <list_of_products>
+  ],
+  "status": "Success"
+}
+```
+  
+---  
+  
+### GET /products?id=
+
+> Get product by ID
+
+_Request Queries_
+```
+?id=<id>
+```
+
+_Request Header_
+```
+not needed
+```
+
+_Request Body_
+```
+not needed
+```
+
+_Response (200)_
+```
+{
+  "data": {
+    "id": <id>
+    "name": <name>,
+    "description": <description>,
+    "price": <price>,
+    "quantity_in_stock": <quantity_in_stock>
+  },
+  "status": "Success"
+}
+```
+
+_Response (404)_
+```
+{
+  "message": "Data Not Found"
+}
+```
+
+---
+
+### POST /products
+
+> Create product
+
+_Request Header_
+```
+not needed
+```
+
+_Request Body_
+```
+{
+  "name": <name>,
+  "description": <description>,
+  "price": <price>,
+  "quantity_in_stock": <quantity_in_stock>, // optional
+}
+```
+
+_Response (201)_
+```
+{
+  "data": {
+    "id": <id>
+    "name": <name>,
+    "description": <description>,
+    "price": <price>,
+    "quantity_in_stock": <quantity_in_stock>
+  },
+  "status": "Success"
+}
+```
+
+_Response (400 - Validation Error)_
+```
+{
+  "status": "Validation Failed",
+  "message": "\"description\" is required"
+}
+```
+
+_Response (400 - Duplicate Name Product)_
+```
+{
+  "message": "Data with this name already existed"
+}
+```
+
+---
+
+### PUT /products/:id
+
+> Update product (except `quantity_in_stock`)
+
+_Request Params_
+```
+/<id>
+```
+_Request Header_
+```
+not needed
+```
+
+_Request Body_
+```
+{
+  "name": <name>, // optional
+  "description": <description>, //optional
+  "price": <price>, // optional
+}
+```
+
+_Response (200)_
+```
+{
+  "data": {
+    "id": <id>
+    "name": <name>,
+    "description": <description>,
+    "price": <price>,
+    "quantity_in_stock": <quantity_in_stock>
+  },
+  "status": "Success"
+}
+```
+
+_Response (400 - Validation Error)_
+```
+{ 
+  \"quantity_in_stock\" is not allowed
+}
+```
+
+_Response (404 - Error Not Found)_
+```
+{
+  "message": "Data Not Found"
+}
+```
+
+_Response (400 - Duplicate Name Product)_
+```
+{
+  "message": "Data with this name already existed"
+}
+```
+
+---
+
+### DELETE /products/:id
+
+> Delete product
+
+_Request Params_
+```
+/<id>
+```
+
+_Request Header_
+```
+not needed
+```
+
+_Request Body_
+```
+not needed
+```
+
+_Response (200)_
+```
+{
+  "data": [<list_of_products>],
+  "message": "Success"
+}
+```
+
+_Response (404 - Error Not Found)_
+```
+{
+    "message": "Data Not Found"
+}
+```
+  
+---
---
+++ index.js
@@ -0,0 +1,156 @@
+import express from "express";
+import fs from "fs";
+import joi from "joi";
+
+const app = express();
+const PORT = 3000;
+const database = './database/db.json';
+const data = JSON.parse(fs.readFileSync(database));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const handleServerError = (res) => {
+  return res.status(500).json({ message: 'Internal server error' })
+};
+
+const handleClientError = (res, status, message) => {
+  return res.status(status).json({ message });
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello, express-backend's client!");
+});
+
+app.get('/products', (req, res) => {
+  try {
+    const { id } = req.query;
+    if (id) {
+      const filteredData = data["products"].filter((el) => el.id === parseInt(id, 10));
+      if (filteredData.length === 0) {
+        return handleClientError(res, 404, "Data Not Found");
+      }
+      return res.status(200).json({ data: filteredData[0], status: 'Success' });
+
+    } else {
+      // Get all data
+      return res.status(200).json({ data: data["products"], status: 'Success' });
+    }
+  } catch (error) {
+    console.error(error);
+    return handleServerError(res);
+  }
+});
+
+app.post('/products', (req, res) => {
+  try {
+    const newData = req.body;
+
+    const scheme = joi.object({
+      name: joi.string().required(),
+      description: joi.string().required(),
+      price: joi.number().required(),
+      quantity_in_stock: joi.number(),
+    });
+
+    const { error } = scheme.validate(newData);
+    if (error) {
+      res.status(400).json({ status: 'Validation Failed', message: error.details[0].message })
+    }
+    
+    if (data.products.find((el) => el.name.toLowerCase() === newData.name.toLowerCase())) {
+      return handleClientError(res, 400, "Data with this name already existed");
+    }
+
+    const newLastID = data.products[data.products.length - 1]["id"] + 1;
+
+    const newCompleteData = {id: newLastID, quantity_in_stock: 0, ...newData};
+    data["products"].push(newCompleteData);
+    fs.writeFileSync(database, JSON.stringify(data));
+    return res.status(201).json({ data: newCompleteData, status: 'Success' });
+
+  } catch (error) {
+    console.error(error);
+    return handleServerError(res);
+  }
+})
+
+app.put('/products/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const dataRequest = req.body;
+
+    const scheme = joi.object({
+      name: joi.string(),
+      description: joi.string(),
+      price: joi.number(),
+    });
+
+    const { error } = scheme.validate(dataRequest);
+    if (error) {
+      res.status(400).json({ status: 'Validation Failed', message: error.details[0].message })
+    }
+
+    const parsedID = parseInt(id, 10);
+    const dataToBeChanged = data["products"].find((el) => el.id === parsedID);
+    if (!dataToBeChanged) {
+      return handleClientError(res, 404, "Data Not Found");
+    }
+
+    const changedData = {...dataToBeChanged, ...dataRequest};
+    const filteredData = data["products"].filter((el) => el.id !== parsedID);
+    if (filteredData.find((el) => el.name.toLowerCase() === changedData.name.toLowerCase())) {
+      return handleClientError(res, 400, "Data with this name already existed");
+    }
+
+    filteredData.push(changedData);
+    const newOrderedData = orderData(filteredData);
+    data["products"] = newOrderedData;
+    fs.writeFileSync(database, JSON.stringify(data));
+    return res.status(200).json({ data: changedData, status: 'Success' });
+
+  } catch (error) {
+    console.error(error);
+    return handleServerError(res);
+  }
+})
+
+app.delete('/products/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const parsedID = parseInt(id, 10)
+    if (!data["products"].find((el) => el.id === parsedID)) {
+      return handleClientError(res, 404, 'Data Not Found');
+    }
+
+    const filteredData = data["products"].filter((el) => el.id !== parsedID);
+    data["products"] = filteredData;
+
+    fs.writeFileSync(database, JSON.stringify(data));
+    return res.status(200).json({ data: filteredData, status: 'Success' });
+
+  } catch (error) {
+    console.error(error);
+    return handleServerError(res);
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`)
+})
+
+const orderData = (data) => {
+  // The items are sorted from the the lowest ID to the largest ID
+  const customCompare = (a, b) => {
+    if (a.id === b.id) {
+      return 0;
+    } else {
+      return a.id > b.id ? 1 : -1;
+    }
+  }
+
+  data.sort(customCompare)
+  return data;
+}
